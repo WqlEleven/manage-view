27079fc35dd00ae5a8f1d2a14f5f2966f4e88cbd
wql <wql@qq.com>
2019-02-12 15:26:35 +0800

    wql cookie

--- src/main.js
+++ src/main.js
@@ -9,11 +9,11 @@ import 'element-ui/lib/theme-chalk/index.css'; // 默认主题
 import './assets/css/icon.css';
 import './components/common/directives';
 import "babel-polyfill";
-
 Vue.config.productionTip = false
 Vue.use(ElementUI, {
     size: 'small'
 });
+axios.defaults.baseURL = 'http://guanjia.applinzi.com/';
 Vue.prototype.$axios = axios;
 Vue.prototype.$qs = qs;
 
